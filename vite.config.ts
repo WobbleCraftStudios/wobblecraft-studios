44bda4f4bbb7395c51adb257890354976606e279
WobbleCraftStudios <support@wobblecraftstudios.com>
2026-03-15 12:42:15 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/wobblecraft-studios/',deploy
   server: {
     host: "::",
     port: 8080,
